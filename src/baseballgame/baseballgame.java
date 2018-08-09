e5c77c2c6f8965bcb38192ce1a1fecc97fb211b1
tjehd <tjehd@58.225.105.123>
2018-08-09 19:56:45 +0900

    코드리뷰 개선할 점 및 추가 요구사항 추가

--- src/baseballgame/baseballgame.java
+++ src/baseballgame/baseballgame.java
@@ -1,104 +1,145 @@
 package baseballgame;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class baseballgame {
 		public static void main(String[] args) {
+			while(true)
+			{
 			Start start=new Start();
 			start.gameinit();
-			while(true) {
-			start.input();
-			start.nothing();
-			if(start.strike()>=3) {
-				break;
+	
+			while(true)
+			{
+				start.input();
+				start.strikeandball();
+				start.nothing();
+				if(start.strike()>=Start.three) 
+					break;
 			}
-			}
-			System.out.println("축하합니다");
+				System.out.println("축하합니다");
+				System.out.println("새로게임을 시작합니다");
+            }	
 }	
 }
 
 
 class Start{
-	
-		 int[] game=new int[3];
-	       int[]insert=new int[3];
-			void gameinit() {
-			for(int i=0;i<game.length;i++) {
-				game[i]=(int)(Math.random()*9+1);
-			}
-			while(game[0]==game[1]) {
-				game[1]=(int)(Math.random()*9+1);
-			}
-			while(game[0]==game[2] || game[1]==game[2]) {
-				game[2]=(int)(Math.random()*9+1);
-			}
-			
+		static final int three=3;
+		
+		 int[] game=new int[three];
+	     int[]insert=new int[three];
+			void gameinit()
+			{
+				for(int i=0;i<game.length;i++)
+				{
+					game[i]=(int)(Math.random()*9+1);
+				}
+				while(game[0]==game[1])
+				{
+					game[1]=(int)(Math.random()*9+1);
+				}
+				while(game[0]==game[2] || game[1]==game[2])
+				{
+					game[2]=(int)(Math.random()*9+1);
+				}
 			}
 			//숫자 입력하기
 			
-			void input() {
-				while(true) {
-			Scanner sc= new Scanner(System.in);
-			for(int i=0;i<insert.length;i++) {
-				System.out.println((i+1)+"번쨰 수");
-				insert[i]=sc.nextInt();
+			void input()
+			{
+				int receiveNumber;
+				Scanner sc= new Scanner(System.in);
+				System.out.println("숫자3개를 입력하세요");
+				receiveNumber=sc.nextInt();
+				insert[0]=receiveNumber/100;
+				insert[1]=(receiveNumber-insert[0]*100)/10;
+				insert[2]=receiveNumber-insert[0]*100-insert[1]*10;
 			}
-			if(insert[0]!=insert[1] && insert[0]!=insert[2] && insert[1]!=insert[2]) {
-				break;}
-			System.out.println("서로다른값을 입력하세요");
-		    sc.close();
-			}}
+			
 			
 			  //스트라이크
-			int strike() {
-			int s=0;
-			for(int i=0;i<game.length;i++) {
-				
-			    	if(game[i]==insert[i]) {
-					s++;
+			int strike() 
+			{
+				int s=0;
+				for(int i=0;i<game.length;i++)
+				{
+				   	if(game[i]==insert[i])
+				       s++;
 				}
-			}
-			return s;
-			
+				return s;
 			}
 			//볼
-			int ball() {
-			int b=0;
-			for(int i=0;i<game.length;i++) {
-				for(int j=0; j<game.length;j++) {
-					if(game[i]==insert[j] && i!=j) {
+			int ball()
+			{
+				int b=0;
+				for(int i=0,j=0;i<game.length;)
+				{
+					
+					if(game[i]==insert[j] && i!=j)
+					{
 						b++;
 					}
+					if(j<2)
+					{
+						j++;
+					}
+					else
+					{
+						i++;j=0;
+					}
 				}
-			}
-			return b;
+				
+				return b;
 			}
 			
 			//낫싱
-			void nothing() {
-			int out=0;
-			for(int i=0;i<game.length;i++) {
+			void nothing()
+		   {
+				int out=0;
 				
-				for(int j=0; j<game.length;j++) {
-					if(game[i]!=insert[j]) {
+				for(int i=0,j=0;i<game.length;)
+				{
+					
+					if(game[i]!=insert[j])
 						out++;
+					if(j<2)
+					{
+						j++;
+					}
+					else
+					{
+						i++;j=0;
 					}
 				}
+	
+				if(out==9)
+				{
+					System.out.println("낫싱");		
 				}
-			if(out==9) {
-				System.out.println("낫싱");		
-			}
-			else {
-				if(strike()>0 && ball()>0) {
-				System.out.println("스트라이크:"+strike()+"볼:"+ball());}
-				else if(strike()>0) {
-					System.out.println("스트라이크:"+strike());
-				}
-				else if(ball()>0) {
-					System.out.println("볼:"+ball());
-				}
-			}
 			}
 			
-	
+			
+			void strikeandball()
+			{
+				if(strike()>0 && ball()>0)
+				{
+					System.out.println("스트라이크:"+strike()+"볼:"+ball());
+				}
+				else if(strike()>0)
+				{
+						System.out.println("스트라이크:"+strike());
+			    }
+				else if(ball()>0)
+				{
+						System.out.println("볼:"+ball());
+				}
+			}
+			
+			
+			
+			
 }
+
+
